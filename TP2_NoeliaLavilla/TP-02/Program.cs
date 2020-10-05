1daa8ef697b8b65f9e890e291771da5d1a6ec3b8
Noelia Lavilla <noelia.m.lavilla@gmail.com>
2020-10-05 00:25:17 -0300

    Se agrega nombre del alumno en titulo de la consola

--- TP2_NoeliaLavilla/TP-02/Program.cs
+++ TP2_NoeliaLavilla/TP-02/Program.cs
@@ -16,7 +16,7 @@ namespace TP_02_2018
             Console.SetWindowSize(Console.LargestWindowWidth / 2, Console.LargestWindowHeight - 2);
 
             // Nombre del alumno
-            Console.Title = "";
+            Console.Title = "Noelia Lavilla";
 
             Taller taller = new Taller(6);
 
